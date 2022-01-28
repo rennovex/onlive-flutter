90c8ebccb3cf711e708b5e85998a66e5806d03f6
Neeraj-SD <neerajsd04@gmail.com>
2022-01-28 09:05:57 +0530

    get_it injection container added

---
+++ onlive/lib/injection_container.dart
@@ -0,0 +1,60 @@
+import 'package:get_it/get_it.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:http/http.dart' as http;
+
+import 'Features/Auth/data/repositories/user_repository_impl.dart';
+import 'Features/Auth/domain/repositories/user_repository.dart';
+import 'Features/Auth/domain/usecase/post_login.dart';
+import 'Features/Auth/presentation/cubit/auth_cubit.dart';
+import 'core/network/network_info.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  //! Features - Auth
+  // Bloc
+  sl.registerFactory(
+    () => AuthCubit(postLogin: sl()),
+  );
+
+  //UseCases
+  sl.registerLazySingleton(() => PostLogin(sl()));
+
+  // Repository
+  sl.registerLazySingleton<UserRepository>(() => UserRepositoryImpl(sl()));
+
+  // Data sources
+  // sl.registerLazySingleton<PostsRemoteDataSource>(
+  //   () => PostsRemoteDataSourceImpl(
+  //     client: sl(),
+  //   ),
+  // );
+
+  // sl.registerLazySingleton<PostsLocalDataSource>(
+  //   () => PostsLocalDataSourceImpl(
+  //     sharedPreferences: sl(),
+  //   ),
+  // );
+
+  //! Core
+  sl.registerLazySingleton<NetworkInfo>(
+    () => NetworkInfoImpl(
+      connectionChecker: sl(),
+    ),
+  );
+
+  //! External
+  // final sharedPreferences = await SharedPreferences.getInstance();
+  // sl.registerLazySingleton(() => sharedPreferences);
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => InternetConnectionChecker());
+  sl.registerFactory(
+    () => GoogleSignIn(
+      scopes: [
+        'email',
+        'https://www.googleapis.com/auth/contacts.readonly',
+      ],
+    ),
+  );
+}
