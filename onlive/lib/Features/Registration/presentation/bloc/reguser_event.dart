300d39103f49672a54470a6a3938907974db31ec
Neeraj-SD <neerajsd04@gmail.com>
2022-01-30 10:04:51 +0530

    regUser bloc added

---
+++ onlive/lib/Features/Registration/presentation/bloc/reguser_event.dart
@@ -0,0 +1,26 @@
+part of 'reguser_bloc.dart';
+
+abstract class ReguserEvent extends Equatable {
+  const ReguserEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class NicknameChanged extends ReguserEvent {
+  final String nickname;
+
+  NicknameChanged(this.nickname);
+
+  @override
+  List<Object> get props => [nickname];
+}
+
+class InterestSelected extends ReguserEvent {
+  final interest;
+
+  InterestSelected(this.interest);
+
+  @override
+  List<Object> get props => [interest];
+}
---
+++ onlive/lib/Features/Registration/presentation/bloc/reguser_state.dart
@@ -0,0 +1,69 @@
+part of 'reguser_bloc.dart';
+
+enum PageStatus { Initial, Loading, Loaded, error }
+
+class ReguserState extends Equatable {
+  String nickname;
+  String interest;
+  String imageUrl;
+  String image;
+  String fullName;
+  int age;
+  String email;
+  String gender;
+  String domain;
+  String college;
+
+  PageStatus avatarSelectionPageState;
+  PageStatus collegeSelectionPageState;
+
+  ReguserState({
+    required this.nickname,
+    required this.interest,
+    required this.imageUrl,
+    required this.image,
+    required this.fullName,
+    required this.age,
+    required this.email,
+    required this.gender,
+    required this.domain,
+    required this.college,
+    required this.avatarSelectionPageState,
+    required this.collegeSelectionPageState,
+  });
+
+  @override
+  List<Object> get props => [];
+
+  ReguserState copyWith({
+    String? nickname,
+    String? interest,
+    String? imageUrl,
+    String? image,
+    String? fullName,
+    int? age,
+    String? email,
+    String? gender,
+    String? domain,
+    String? college,
+    PageStatus? avatarSelectionPageState,
+    PageStatus? collegeSelectionPageState,
+  }) {
+    return ReguserState(
+      nickname: nickname ?? this.nickname,
+      interest: interest ?? this.interest,
+      imageUrl: imageUrl ?? this.imageUrl,
+      image: image ?? this.image,
+      fullName: fullName ?? this.fullName,
+      age: age ?? this.age,
+      email: email ?? this.email,
+      gender: gender ?? this.gender,
+      domain: domain ?? this.domain,
+      college: college ?? this.college,
+      avatarSelectionPageState:
+          avatarSelectionPageState ?? this.avatarSelectionPageState,
+      collegeSelectionPageState:
+          collegeSelectionPageState ?? this.collegeSelectionPageState,
+    );
+  }
+}
