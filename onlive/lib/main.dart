a172932e3995f9efb6c7996ace99283d6831e02e
Neeraj-SD <neerajsd04@gmail.com>
2022-02-11 17:50:43 +0530

    flutter bottom navigation added

--- onlive/lib/main.dart
+++ onlive/lib/main.dart
@@ -39,7 +39,78 @@ class MyApp extends StatelessWidget {
             create: (context) => di.sl<AuthCubit>(),
           )
         ],
-        child: ChatOverviewScreen(),
+        child: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    ChatOverviewScreen(),
+    Center(
+      child: Text(
+        'Groups',
+        style: optionStyle,
+      ),
+    ),
+    Center(
+      child: Text(
+        'Questions',
+        style: optionStyle,
+      ),
+    ),
+    Center(
+      child: Text(
+        'Settings',
+        style: optionStyle,
+      ),
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chats',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Groups',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'Questions',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
       ),
     );
   }
